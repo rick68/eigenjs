cef9c362aac78f7d1eb0bff73b1de489a2566ef1
Rick Yang <rick68@gmail.com>
2014-07-26 19:41:28 +0800

    test: test methods of Matrix 'rows()' and 'cols()'

--- test/Matrix.js
+++ test/Matrix.js
@@ -3,6 +3,8 @@ const
     should = require('should');
 
 describe('Matrix', function() {
+  var mat;
+
   it('#Matrix() should be a function', function() {
     Matrix.should.be.a.Function;
   });
@@ -17,4 +19,10 @@ describe('Matrix', function() {
     Matrix(1,2).should.be.an.Object;
     (new Matrix(3, 4)).should.be.an.Object;
   });
+
+  it('#Matrix(3, 2) should return the matrix of size 3x2', function() {
+    mat = Matrix(3, 2);
+    mat.rows().should.equal(3);
+    mat.cols().should.equal(2);
+  })
 });
