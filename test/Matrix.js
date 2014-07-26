98fb775c2c455d15da77d4679f6b9064fc6b7b27
Rick Yang <rick68@gmail.com>
2014-07-26 18:25:07 +0800

    test: test Matrix's constructor

--- test/Matrix.js
+++ test/Matrix.js
@@ -1,16 +1,20 @@
 const
-    Eigen = require('../index.js');
+    Matrix = require('../index.js').Matrix;
     should = require('should');
 
-describe('Eigen', function () {
-  describe('Matrix', function() {
-    it('#Matrix() should be a function', function() {
-      Eigen.Matrix.should.be.a.Function;
-    });
+describe('Matrix', function() {
+  it('#Matrix() should be a function', function() {
+    Matrix.should.be.a.Function;
+  });
 
-    it('#Matrix() should return an object', function() {
-      Eigen.Matrix().should.be.an.Object;
-      (new Eigen.Matrix()).should.be.an.Object;
-    });
+  it('should throw error when tried creating matrix without rows and columns arguments', function() {
+    (function() { new Matrix(); }).should.throw();
+    (function() { new Matrix(1); }).should.throw();
+    (function() { new Matrix(1, 2); }).should.not.throw();
+  });
+
+  it('should be invoked with arguments rows and columns and return an object', function() {
+    Matrix(1,2).should.be.an.Object;
+    (new Matrix(3, 4)).should.be.an.Object;
   });
 });
