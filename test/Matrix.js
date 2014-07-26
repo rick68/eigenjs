537b748a97c0f8b9a903dbb6bb2f7d06b01f9fa3
Rick Yang <rick68@gmail.com>
2014-07-26 22:25:02 +0800

    test: test Matrix methods 'set()' and 'get()'

--- test/Matrix.js
+++ test/Matrix.js
@@ -25,4 +25,30 @@ describe('Matrix', function() {
     mat.rows().should.equal(3);
     mat.cols().should.equal(2);
   })
+
+  beforeEach(function() {
+    mat = Matrix(3, 3);
+    mat.set(0, 0, 1);
+    mat.set(0, 1, 2);
+    mat.set(0, 2, 3);
+    mat.set(1, 0, 4);
+    mat.set(1, 1, 5);
+    mat.set(1, 2, 6);
+    mat.set(2, 0, 7);
+    mat.set(2, 1, 8);
+    mat.set(2, 2, 9);
+  });
+
+  it('#get() should return the element value of Matrix', function() {
+    mat.get(0, 0).should.equal(1);
+    mat.get(0, 1).should.equal(2);
+    mat.get(0, 2).should.equal(3);
+    mat.get(1, 0).should.equal(4);
+    mat.get(1, 1).should.equal(5);
+    mat.get(1, 2).should.equal(6);
+    mat.get(2, 0).should.equal(7);
+    mat.get(2, 1).should.equal(8);
+    mat.get(2, 2).should.equal(9);
+    mat.get().should.equal("1 2 3\n4 5 6\n7 8 9");
+  })
 });
