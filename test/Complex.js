a904f2b40222a53c26246b8ec7d9329fe4a15877
Rick Yang <rick68@gmail.com>
2014-07-31 22:26:31 +0800

    test: add 'Complex.real' testing

--- test/Complex.js
+++ test/Complex.js
@@ -12,4 +12,12 @@ describe('Complex', function() {
   it('#toString() should convert object to string', function() {
     c.toString().should.equal("(3,-4)");
   });
+
+  it('has a property `real`', function() {
+    c.should.have.property('real');
+    c.real.should.equal(3);
+    c.real = 7;
+    c.real.should.equal(7);
+    c.toString().should.equal("(7,-4)");
+  });
 });
