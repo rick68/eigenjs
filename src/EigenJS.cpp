951811f15cb0e9b8ca2535bc4430cc85ffd90ffe
Wei-Ming Yang <rick68@gmail.com>
2014-07-31 08:41:13 +0800

    src: fixed a problem in g++ with C++11

--- src/EigenJS.cpp
+++ src/EigenJS.cpp
@@ -21,8 +21,8 @@
 
 namespace EigenJS {
 
-static const char matrix_class_name[] = "Matrix";
-// static const char cmatrix_class_name[] = "CMatrix";
+static constexpr char matrix_class_name[] = "Matrix";
+// static constexpr char cmatrix_class_name[] = "CMatrix";
 
 void Init(v8::Handle<v8::Object> exports) {
   Matrix<double, matrix_class_name>::Init(exports);
