12fcc22d5db3cffd804013740dd98309f181106c
Rick Yang <rick68@gmail.com>
2014-08-04 21:11:22 +0800

    src: added 'Complex_fwd.hpp'

---
+++ src/Complex_fwd.hpp
@@ -0,0 +1,26 @@
+//
+// Complex_fwd.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2014 Rick Yang (rick68 at gmail dot com)
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#ifndef EIGENJS_COMPLEX_FWD_HPP
+#define EIGENJS_COMPLEX_FWD_HPP
+
+#include <boost/config.hpp>
+
+namespace EigenJS {
+
+BOOST_CONSTEXPR char complex_class_name[] = "Complex";
+
+template <typename ValueType, const char* ClassName = complex_class_name>
+class Complex;
+
+}  // namespace EigenJS
+
+#endif  // EIGENJS_COMPLEX_FWD_HPP
