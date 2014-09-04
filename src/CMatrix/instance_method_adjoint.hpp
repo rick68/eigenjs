b82847255bc8d2644e379b71b9aa0af1fadadffd
Rick Yang <rick68@gmail.com>
2014-09-04 22:22:23 +0800

    src: added instance method 'adjoint()'

---
+++ src/CMatrix/instance_method_adjoint.hpp
@@ -0,0 +1,26 @@
+//
+// CMatrix/instance_method_adjoint.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2014 Rick Yang (rick68 at gmail dot com)
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#ifndef EIGENJS_CMATRIX_INSTANCE_METHOD_ADJOINT_HPP
+#define EIGENJS_CMATRIX_INSTANCE_METHOD_ADJOINT_HPP
+
+#include "../common_macro.hpp"
+
+namespace EigenJS {
+
+EIGENJS_INSTANCE_METHOD(CMatrix, adjoint,
+{
+  EIGENJS_COMMON_MATRIX_INSTANCE_METHOD_ADJOINT_CONTEXT()
+})
+
+}  // namespace EigenJS
+
+#endif  // EIGENJS_CMATRIX_INSTANCE_METHOD_ADJOINT_HPP
--- src/Matrix/definitions.hpp
+++ src/Matrix/definitions.hpp
@@ -37,6 +37,7 @@
 #include "instance_method_diva.hpp"
 
 #include "instance_method_transpose.hpp"
+#include "instance_method_adjoint.hpp"
 #include "instance_method_determinant.hpp"
 #include "instance_method_inverse.hpp"
 
@@ -75,9 +76,10 @@ EIGENJS_OBJECT_DEFINITIONS(
   (instance_method_div)
   (instance_method_diva)
 
+  (instance_method_transpose)
+  (instance_method_adjoint)
   (instance_method_determinant)
   (instance_method_inverse)
-  (instance_method_transpose)
 
   (instance_method_equals)
   (instance_method_isApprox)
