0261e3dc07919c2378607d89a9df39fd85b23ea2
Rick Yang <rick68@gmail.com>
2014-09-20 09:27:44 +0800

    src: add instance method 'bottomRows()'

--- src/CMatrix/definitions.hpp
+++ src/CMatrix/definitions.hpp
@@ -35,6 +35,7 @@
 #include "instance_method_row.hpp"
 #include "instance_method_col.hpp"
 #include "instance_method_topRows.hpp"
+#include "instance_method_bottomRows.hpp"
 
 #include "instance_method_replicate.hpp"
 
@@ -107,6 +108,7 @@ EIGENJS_OBJECT_DEFINITIONS(
   (instance_method_row)
   (instance_method_col)
   (instance_method_topRows)
+  (instance_method_bottomRows)
 
   (instance_method_replicate)
 
---
+++ src/CMatrix/instance_method_bottomRows.hpp
@@ -0,0 +1,56 @@
+//
+// CMatrix/instance_method_bottomRows.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2014 Rick Yang (rick68 at gmail dot com)
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#ifndef EIGENJS_CMATRIX_INSTANCE_METHOD_BOTTOMROWS_HPP
+#define EIGENJS_CMATRIX_INSTANCE_METHOD_BOTTOMROWS_HPP
+
+namespace EigenJS {
+
+EIGENJS_INSTANCE_METHOD(CMatrix, bottomRows,
+{
+  NanScope();
+
+  if (args.Length() == 1) {
+    if (args[0]->IsNumber()) {
+      const T* obj = node::ObjectWrap::Unwrap<T>(args.This());
+      const typename T::value_type& value = **obj;
+      const typename T::value_type::Index& n = args[0]->Int32Value();
+
+      if (n <= 0 || n > value.rows()) {
+        EIGENJS_THROW_ERROR_INVALID_ARGUMENT()
+        NanReturnUndefined();
+      }
+
+      v8::Local<v8::Value> argv[] = {
+          args.This()
+        , NanNew<v8::Integer>(value.rows() - n)  /* startRow */
+        , NanNew<v8::Integer>(0)                 /* startCol */
+        , NanNew<v8::Integer>(n)                 /* blockRows */
+        , NanNew<v8::Integer>(value.cols())      /* blockCols */
+      };
+
+      NanReturnValue(
+        CMatrixBlock::new_instance(
+          args
+        , sizeof(argv) / sizeof(v8::Local<v8::Value>)
+        , argv
+        )
+      );
+    }
+  }
+
+  EIGENJS_THROW_ERROR_INVALID_ARGUMENT()
+  NanReturnUndefined();
+})
+
+}  // namespace EigenJS
+
+#endif  // EIGENJS_CMATRIX_INSTANCE_METHOD_BOTTOMROWS_HPP
