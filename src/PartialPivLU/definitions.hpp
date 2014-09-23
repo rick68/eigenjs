5de60fbba2e9c626f07ec539db36163be69aa299
Rick Yang <rick68@gmail.com>
2014-09-24 07:21:53 +0800

    src: add instance method 'permutationP()' of PartialPivLU

---
+++ src/PartialPivLU/definitions.hpp
@@ -0,0 +1,28 @@
+//
+// PartialPivLU/definitions.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2014 Rick Yang (rick68 at gmail dot com)
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#ifndef EIGENJS_PARTIALPIVLU_DEFINITIONS_HPP
+#define EIGENJS_PARTIALPIVLU_DEFINITIONS_HPP
+
+#include "../PartialPivLU_fwd.hpp"
+
+#include "instance_method_permutationP.hpp"
+
+namespace EigenJS {
+
+EIGENJS_OBJECT_DEFINITIONS(
+  PartialPivLU
+, (instance_method_permutationP)
+);
+
+}  // namespace EigenJS
+
+#endif  // EIGENJS_PARTIALPIVLU_DEFINITIONS_HPP
