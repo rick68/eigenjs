8fb8cf8f54a5f629ec4f07debed503035be38bea
Rick Yang <rick68@gmail.com>
2014-10-02 23:14:00 +0800

    test: add instance method 'dimensionOfKernel()' testings

--- test/CFullPivLU.js
+++ test/CFullPivLU.js
@@ -97,4 +97,14 @@ describe('CFullPivLU', function() {
     lu = Matrix.Random(3, 3);
     lu.inverse();
   });
+
+  it('#dimensionOfKernel() should return the dimension of the kernel of the matrix of which *this is the LU decomposition.', function() {
+    lu.dimensionOfKernel.should.be.a.Function;
+
+    lu.dimensionOfKernel().should.equal(2);
+
+    CMatrix.Random(2, 2).fullPivLu().dimensionOfKernel().should.equal(0);
+    CMatrix.Random(2, 3).fullPivLu().dimensionOfKernel().should.equal(1);
+    CMatrix.Random(2, 4).fullPivLu().dimensionOfKernel().should.equal(2);
+  });
 });
