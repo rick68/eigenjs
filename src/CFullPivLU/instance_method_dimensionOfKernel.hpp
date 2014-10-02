0683ecb3d4302cafcc1f001828bf3ad0e5553e4f
Rick Yang <rick68@gmail.com>
2014-10-02 23:13:28 +0800

    src: add instance method 'dimensionOfKernel()' for FullPivLU and CFullPivLU

---
+++ src/CFullPivLU/instance_method_dimensionOfKernel.hpp
@@ -0,0 +1,28 @@
+//
+// CFullPivLU/instance_method_dimensionOfKernel.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2014 Rick Yang (rick68 at gmail dot com)
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#ifndef EIGENJS_CFULLPIVLU_INSTANCE_METHOD_DIMENSIONOFKERNEL_HPP
+#define EIGENJS_CFULLPIVLU_INSTANCE_METHOD_DIMENSIONOFKERNEL_HPP
+
+namespace EigenJS {
+
+EIGENJS_INSTANCE_METHOD(CFullPivLU, dimensionOfKernel,
+{
+  const T* const& obj = node::ObjectWrap::Unwrap<T>(args.This());
+  const typename T::value_type& value = **obj;
+
+  NanScope();
+  NanReturnValue(NanNew<v8::Integer>(value.dimensionOfKernel()));
+})
+
+}  // namespace EigenJS
+
+#endif  // EIGENJS_CFULLPIVLU_INSTANCE_METHOD_DIMENSIONOFKERNEL_HPP
